3c611e9bdf2af7ade52de80acc5dad819754551f
EC2 Default User <ec2-user@ip-172-31-1-143.ap-northeast-1.compute.internal>
2024-06-16 23:49:50 +0000

    phase1

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,17 @@
 class Post < ApplicationRecord
   has_one_attached :image
-  
-  # has_one :restaurant, dependent: :destroy
+
   belongs_to :user
   belongs_to :genre
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   validates :dish, presence: true
   validates :impression, presence: true
   validates :restaurant_name, presence: true
   validates :menu, presence: true
   validates :address, presence: true
-  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
