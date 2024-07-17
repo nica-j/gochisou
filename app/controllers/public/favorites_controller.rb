8bdd5ed7a1b81d34c6c999906adc7d1ca660648c
EC2 Default User <ec2-user@ip-172-31-1-143.ap-northeast-1.compute.internal>
2024-07-17 12:27:09 +0000

    fix

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -9,8 +9,8 @@ class Public::FavoritesController < ApplicationController
 
   def index
     @user = User.find(params[:id])
-    favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
-    @favorite_posts = Post.find(favorites)
+    @favorites = Favorite.where(user_id: @user.id).pluck(:post_id)
+    @favorite_posts = Post.find(@favorites)
     @post = Post.find(params[:id])
   end
 
